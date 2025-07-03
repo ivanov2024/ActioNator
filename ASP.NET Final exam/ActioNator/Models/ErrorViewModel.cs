846e13a0b7d4cdd9e9f5ee47cff091d6f2a70281
Ivan Ivanov <ivanov.github@gmail.com>
2025-07-03 20:27:11 +0300

    Added a new ASP.NET Core Web App (MVC) project

---
+++ ASP.NET Final exam/ActioNator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ActioNator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
