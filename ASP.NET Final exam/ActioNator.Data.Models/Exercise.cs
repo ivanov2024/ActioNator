b968caee386499c5c0fd2b2f4e912e58961c295b
Ivan Ivanov <ivanov.github@gmail.com>
2025-07-04 10:59:59 +0300

    Added Exercise model and validation constants
    Introduced the Exercise entity in the data models with appropriate data annotations for validation. Added corresponding validation constants for Exercise in ValidationConstants.cs to centralize and standardize validation rules.

---
+++ ASP.NET Final exam/ActioNator.Data.Models/Exercise.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static ActioNator.GCommon.ValidationConstants.Exercise;
+
+namespace ActioNator.Data.Models
+{
+    public class Exercise
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        public Guid WorkoutId { get; set; }
+
+        [Required]
+        public virtual Workout Workout { get; set; } = null!;
+
+        [Required]
+        [MinLength(NameMinLength)]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Range(MinSets, MaxSets)]
+        public int Sets { get; set; }
+
+        [Range(MinReps, MaxReps)]
+        public int Reps { get; set; }
+
+        [Range(MinWeight, MaxWeight)]
+        public decimal Weight { get; set; }
+
+        [MinLength(NotesMinLength)]
+        [MaxLength(NotesMaxLength)]
+        public string? Notes { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+    }
+}
--- ASP.NET Final exam/ActioNator.GCommon/ValidationConstants.cs
+++ ASP.NET Final exam/ActioNator.GCommon/ValidationConstants.cs
@@ -10,5 +10,23 @@
             public const int DescriptionMinLength = 10;
             public const int DescriptionMaxLength = 250;
         }
+
+        public static class Exercise
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 300;
+
+            public const int MinSets = 1;
+            public const int MaxSets = 100;
+
+            public const int MinReps = 1;
+            public const int MaxReps = 100;
+
+            public const int MinWeight = 0;
+            public const int MaxWeight = 1000;
+
+            public const int NotesMinLength = 0;
+            public const int NotesMaxLength = 500;
+        }
     }
 }
