5dbf66cc74542d9030bf7bd2ba8dd22d926a996d
Ivan Ivanov <ivanov.github@gmail.com>
2025-07-09 22:32:24 +0300

    Added ExerciseTemplate entity configuration
    Introduced ExerciseTemplateConfiguration to define the primary key for the ExerciseTemplate entity using Entity Framework Core.

---
+++ ASP.NET Final exam/ActioNator.Data/EntityConfigurations/ExerciseTemplateConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using ActioNator.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ActioNator.Data.EntityConfigurations
+{
+    internal class ExerciseTemplateConfiguration : IEntityTypeConfiguration<ExerciseTemplate>
+    {
+        public void Configure(EntityTypeBuilder<ExerciseTemplate> exerciseTemplate)
+        {
+            exerciseTemplate
+                .HasKey(et => et.Id);
+        }
+    }
+}
