7215824195ac19c51e7293b80bfb29c927dfe028
Ivan Ivanov <ivanov.github@gmail.com>
2025-07-09 22:45:47 +0300

    Added Goal entity configuration
    Introduced GoalConfiguration to define entity relationships, default values, and query filters for the Goal model in Entity Framework Core.

---
+++ ASP.NET Final exam/ActioNator.Data/EntityConfigurations/GoalConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using ActioNator.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ActioNator.Data.EntityConfigurations
+{
+    internal class GoalConfiguration : IEntityTypeConfiguration<Goal>
+    {
+        public void Configure(EntityTypeBuilder<Goal> goal)
+        {
+            goal
+                .HasKey(g => g.Id);
+
+            goal
+                .HasOne(g => g.ApplicationUser)
+                .WithMany(au => au.Goals)
+                .HasForeignKey(g => g.ApplicationUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            goal
+                .Property(g => g.CreatedAt)
+                .HasDefaultValue(DateTime.UtcNow);
+
+            goal
+                .Property(g => g.DueDate)
+                .IsRequired(false);
+
+            goal
+                .Property(g => g.IsCompleted)
+                .HasDefaultValue(false);
+
+            goal
+                .Property(g => g.CompletedAt)
+                .IsRequired(false);
+
+            goal
+                .Property(g => g.IsDeleted)
+                .HasDefaultValue(false);
+
+            goal
+                .HasQueryFilter(g => g.IsDeleted == false);
+        }
+    }
+}
