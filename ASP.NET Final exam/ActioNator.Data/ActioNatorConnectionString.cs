8a3c824f19140609fde44b08ae9176e1b31f46bf
Ivan Ivanov <ivanov.github@gmail.com>
2025-07-10 19:20:55 +0300

    Added connection string and updated ActioNatorDbContext
    Introduced ActioNatorConnectionString to centralize the database connection string. Updated ActioNatorDbContext to inherit from IdentityDbContext, added DbSet properties for entities, and configured OnConfiguring and OnModelCreating methods for EF Core setup.

---
+++ ASP.NET Final exam/ActioNator.Data/ActioNatorConnectionString.cs
@@ -0,0 +1,8 @@
+﻿namespace ActioNator.Data
+{
+    internal class ActioNatorConnectionString
+    {
+        internal static readonly string ConnectionString
+            = "Server =.;Database=ActioNator;TrustServerCertificate=True;TrustServerCertificate=True;";
+    }
+}
--- ASP.NET Final exam/ActioNator.Data/ActioNatorDbContext.cs
+++ ASP.NET Final exam/ActioNator.Data/ActioNatorDbContext.cs
@@ -1,7 +1,56 @@
-﻿namespace ActioNator.Data
-{
-    public class ActioNatorDbContext
-    {
+﻿using ActioNator.Data.EntityConfigurations;
+using ActioNator.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using static ActioNator.Data.ActioNatorConnectionString;
 
+namespace ActioNator.Data
+{
+    public class ActioNatorDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        public ActioNatorDbContext() { }
+
+        public ActioNatorDbContext(DbContextOptions<ActioNatorDbContext> options)
+            : base(options) { }
+
+        public DbSet<Achievement> Achievements { get; set; } = null!;
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; } = null!;
+
+        public DbSet<Chat> Chats { get; set; } = null!;
+
+        public DbSet<Comment> Comments { get; set; } = null!;
+
+        public DbSet<Exercise> Exercises { get; set; } = null!;
+
+        public DbSet<ExerciseTemplate> ExerciseTemplates { get; set; } = null!;
+
+        public DbSet<Goal> Goals { get; set; } = null!;
+
+        public DbSet<JournalEntry> JournalEntries { get; set; } = null!;
+
+        public DbSet<Message> Messages { get; set; } = null!;
+
+        public DbSet<Post> Posts { get; set; } = null!;
+
+        public DbSet<Workout> Workouts { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(ConnectionString);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base
+                .OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .ApplyConfigurationsFromAssembly(typeof(ApplicationUserConfiguration).Assembly);
+        }
     }
 }
