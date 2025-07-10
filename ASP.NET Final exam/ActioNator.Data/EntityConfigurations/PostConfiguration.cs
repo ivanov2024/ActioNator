15d51c22e0e7806771321659ffae272b84bd293d
Ivan Ivanov <ivanov.github@gmail.com>
2025-07-10 10:26:57 +0300

    Added Post entity configuration and require Title
    Added PostConfiguration class to define EF Core entity configuration for Post, including relationships, property defaults, and query filters. Also marked the Title property in Post as [Required] for validation.

---
+++ ASP.NET Final exam/ActioNator.Data/EntityConfigurations/PostConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using ActioNator.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ActioNator.Data.EntityConfigurations
+{
+    internal class PostConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> post)
+        {
+            post
+                .HasKey(p => p.Id);
+
+            post
+                .HasOne(p => p.ApplicationUser)
+                .WithMany(u => u.Posts)
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            post
+                .Property(p => p.Content)
+                .IsRequired(false);
+
+            post
+                .Property(p => p.CreatedAt)
+                .HasDefaultValue(DateTime.UtcNow);
+
+            post
+                .Property(p => p.ImageUrl)
+                .IsRequired(false);
+
+            post
+                .Property(p => p.IsPublic)
+                .HasDefaultValue(false);
+
+            post
+                .Property(p => p.IsDeleted)
+                .HasDefaultValue(false);
+
+            post
+                .HasQueryFilter(p => p.IsPublic && !p.IsDeleted);
+        }
+    }
+}
