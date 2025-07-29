c6fbc5020c78683486d93771de02cc59b313231b
Ivan Ivanov <ivanov.github@gmail.com>
2025-07-29 18:47:36 +0300

    Refactored error handling with ErrorDetailsModel
    Introduced ErrorDetailsModel to encapsulate detailed error information and replaced the previous ErrorDetails class in FileUploadResponse. This centralizes error handling logic and improves maintainability.

--- ASP.NET Final exam/ActioNator.Services/Models/FileUploadModels.cs
+++ ASP.NET Final exam/ActioNator.Services/Models/FileUploadModels.cs
@@ -8,12 +8,13 @@ namespace ActioNator.Services.Models
         /// <summary>
         /// List of uploaded file paths
         /// </summary>
-        public List<UploadedFileInfo> Files { get; set; } = new List<UploadedFileInfo>();
+        public List<UploadedFileInfo> Files { get; set; } 
+            = new List<UploadedFileInfo>();
         
         /// <summary>
         /// Error details if the operation failed
         /// </summary>
-        public ErrorDetails? Error { get; set; }
+        public ErrorDetailsModel? Error { get; set; }
         
         /// <summary>
         /// Creates a successful response
@@ -37,65 +38,14 @@ namespace ActioNator.Services.Models
         /// <param name="message">Error message</param>
         /// <param name="errorDetails">Detailed error information</param>
         /// <returns>FileUploadResponse instance</returns>
-        public static FileUploadResponse CreateFailure(string message, ErrorDetails? errorDetails = null)
+        public static FileUploadResponse CreateFailure(string message, ErrorDetailsModel? errorDetails = null)
         {
             return new FileUploadResponse
             {
                 Success = false,
                 Message = message,
-                Error = errorDetails ?? new ErrorDetails { ErrorType = "UnknownError", ErrorMessage = message }
+                Error = errorDetails ?? new ErrorDetailsModel { ErrorType = "UnknownError", ErrorMessage = message }
             };
-        }
-    }
-    
-    /// <summary>
-    /// Detailed error information
-    /// </summary>
-    public class ErrorDetails
-    {
-        /// <summary>
-        /// Type of error
-        /// </summary>
-        public required string ErrorType { get; set; }
-        
-        /// <summary>
-        /// Error message
-        /// </summary>
-        public required string ErrorMessage { get; set; }
-        
-        /// <summary>
-        /// Additional error details
-        /// </summary>
-        public Dictionary<string, object> AdditionalInfo { get; set; } = new Dictionary<string, object>();
-        
-        /// <summary>
-        /// Creates error details from an exception
-        /// </summary>
-        /// <param name="exception">Exception to create error details from</param>
-        /// <returns>ErrorDetails instance</returns>
-        public static ErrorDetails FromException(Exception exception)
-        {
-            if (exception == null)
-            {
-                return new ErrorDetails
-                {
-                    ErrorType = "UnknownError",
-                    ErrorMessage = "An unknown error occurred"
-                };
-            }
-            
-            var errorDetails = new ErrorDetails
-            {
-                ErrorType = exception.GetType().Name,
-                ErrorMessage = exception.Message
-            };
-            
-            // Add stack trace in development environments only
-            #if DEBUG
-            errorDetails.AdditionalInfo["StackTrace"] = exception.StackTrace;
-            #endif
-            
-            return errorDetails;
         }
     }
 }
