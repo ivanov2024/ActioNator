ea0151c8cc9bb446ead37ec5039101d82016196d
Ivan Ivanov <ivanov.github@gmail.com>
2025-08-08 12:45:13 +0300

    Code refactoring
    Seperated extracting Cloudinary public ID logic from ICloudinaryService into its own service ICloudinaryUrlService for following SRP and ISP

--- ASP.NET Final exam/ActioNator.Services/Interfaces/Cloud/ICloudinaryService.cs
+++ ASP.NET Final exam/ActioNator.Services/Interfaces/Cloud/ICloudinaryService.cs
@@ -3,28 +3,47 @@ using System.Threading.Tasks;
 
 namespace ActioNator.Services.Interfaces.Cloud
 {
+    /// <summary>
+    /// Defines operations for uploading and deleting images in Cloudinary
+    /// that are associated with user posts.
+    /// </summary>
     public interface ICloudinaryService
     {
         /// <summary>
-        /// Uploads an image to Cloudinary
+        /// Uploads a single image to Cloudinary and associates it with a specific post.
         /// </summary>
-        /// <param name="file">The image file to upload</param>
-        /// <param name="folder">Optional folder name to organize uploads</param>
-        /// <returns>The secure URL of the uploaded image</returns>
-        Task<string> UploadImageAsync(IFormFile file, Guid postId, string folder = "community");
-        
+        /// <param name="image">The image file to upload.</param>
+        /// <param name="postId">The unique identifier of the post to associate the image with.</param>
+        /// <param name="folder">
+        /// Optional Cloudinary folder name for organizing uploads.
+        /// Defaults to "community".
+        /// </param>
+        /// <returns>
+        /// The secure URL of the uploaded image.
+        /// </returns>
+        Task<string> UploadImageAsync(IFormFile image, Guid postId, string folder = "community");
+
         /// <summary>
-        /// Deletes an image from Cloudinary
+        /// Uploads multiple images to Cloudinary and associates them with a specific post.
         /// </summary>
-        /// <param name="publicId">The public ID of the image to delete</param>
-        /// <returns>True if deletion was successful</returns>
-        Task<bool> DeleteImageAsync(string publicId);
-        
+        /// <param name="images">The collection of image files to upload.</param>
+        /// <param name="postId">The unique identifier of the post to associate the images with.</param>
+        /// <param name="folder">
+        /// Optional Cloudinary folder name for organizing uploads.
+        /// Defaults to "community".
+        /// </param>
+        /// <returns>
+        /// A collection of secure URLs of the uploaded images.
+        /// </returns>
+        Task<IEnumerable<string>> UploadImagesAsync(IEnumerable<IFormFile> images, Guid postId, string folder = "community");
+
         /// <summary>
-        /// Extracts the public ID from a Cloudinary URL
+        /// Deletes all images from Cloudinary that are associated with a specific post.
         /// </summary>
-        /// <param name="cloudinaryUrl">The Cloudinary URL</param>
-        /// <returns>The public ID</returns>
-        string GetPublicIdFromUrl(string cloudinaryUrl);
+        /// <param name="postId">The unique identifier of the post whose images should be deleted.</param>
+        /// <returns>
+        /// True if all deletions were successful; otherwise, false.
+        /// </returns>
+        Task<bool> DeleteImagesByPostIdAsync(Guid postId);
     }
 }
---
+++ ASP.NET Final exam/ActioNator.Services/Interfaces/Cloud/ICloudinaryUrlService.cs
@@ -0,0 +1,18 @@
+﻿namespace ActioNator.Services.Interfaces.Cloud
+{
+    /// <summary>
+    /// Defines operations for extracting data from Cloudinary URLs.
+    /// </summary>
+    public interface ICloudinaryUrlService
+    {
+        /// <summary>
+        /// Extracts the Cloudinary public ID from a given Cloudinary image URL.
+        /// </summary>
+        /// <param name="cloudinaryUrl">The full Cloudinary URL of the image.</param>
+        /// <returns>
+        /// The public ID string that uniquely identifies the image in Cloudinary.
+        /// </returns>
+        string GetPublicId(string cloudinaryUrl);
+    }
+
+}
